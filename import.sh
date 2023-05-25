ea6e144bc2ca71fee59249cd4f6a3edd92e8a3c9
Ak47Twex <124576368+Doplx@users.noreply.github.com>
2023-05-25 17:26:09 +0300

    Update import.sh

--- import.sh
+++ import.sh
@@ -4,7 +4,7 @@
 
 # rm -f depictions/* sileodepictions/*
 python3 gen/depiction.py
-dpkg-scanpackages -m /debs /dev/null > Packages
+dpkg-scanpackages -m ./debs /dev/null > Packages
 gzip -cf Packages > Packages.gz
 xz -9fkev Packages > Packages.xz
 bzip2 -cf Packages > Packages.bz2
